469a3b54f2a2055d7fb1f648515dbf821353968a
Adele-Roos <121452950+Adele-Roos@users.noreply.github.com>
2023-10-18 09:48:07 +0200

    Initial commit

---
+++ src/components/Dictionary.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+function DictionaryApp() {
+  const [word, setWord] = useState("");
+  const [definition, setDefinition] = useState("");
+  const [example, setExample] = useState("");
+  const apiKey = "";
+}
